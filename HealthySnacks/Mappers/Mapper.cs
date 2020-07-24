22cf8b4d1645fa2d26c72ab343d99a896feb0535
Aika <aiganysha@gmail.com>
2020-07-23 21:45:47 -0500

    Added Mapper folder

---
+++ HealthySnacks/Mappers/Mapper.cs
@@ -0,0 +1,32 @@
+﻿namespace HealthySnacks.Controllers
+{
+    public class Mapper
+    {
+        public HealthySnacksModel healthySnacksModel(HealthySnacks healthySnacks)
+        {
+            HealthySnacksModel healthySnacksModel = new HealthySnacksModel();
+            healthySnacksModel.Id = healthySnacks.Id;
+            healthySnacksModel.Name = healthySnacks.Name;
+            healthySnacksModel.Description = healthySnacks.Description;
+            healthySnacksModel.Price = healthySnacks.Price;
+            healthySnacksModel.ImageUrl = healthySnacks.ImageUrl;
+            healthySnacksModel.CategoryId = healthySnacks.CategoryId;
+
+            return healthySnacksModel;
+
+        }
+
+        public HealthySnacks MapToHealthySnacks(HealthySnacksModel healthySnacksModel)
+        {
+            HealthySnacks healthySnacks = new HealthySnacks();
+            healthySnacks.Id = healthySnacksModel.Id;
+            healthySnacks.Name = healthySnacksModel.Name;
+            healthySnacks.Description = healthySnacksModel.Description;
+            healthySnacks.Price = healthySnacksModel.Price;
+            healthySnacks.ImageUrl = healthySnacksModel.ImageUrl;
+            healthySnacks.CategoryId = healthySnacksModel.CategoryId;
+
+
+        }
+    }
+}
