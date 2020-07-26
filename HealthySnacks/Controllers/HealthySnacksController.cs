0d27cca6e0fd8c74bfd60f8d3246a7afc13a4390
Aika <aiganysha@gmail.com>
2020-07-25 22:39:45 -0500

    Home Page modified

--- HealthySnacks/Controllers/HealthySnacksController.cs
+++ HealthySnacks/Controllers/HealthySnacksController.cs
@@ -1,5 +1,7 @@
 ﻿using HealthySnacks.DataAccess.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using HealthySnacks.Models;
+using HealthySnacks.DataAccess.Entities;
 
 
 namespace HealthySnacks.Controllers
@@ -22,38 +24,6 @@ namespace HealthySnacks.Controllers
             var healthySnacks = healthySnacksRepository.GetHealthySnacks();
 
             return View();
-        }
-    }
-
-
-
-    public class Mapper
-    {
-        public HealthySnacksModel healthySnacksModel(HealthySnacks healthySnacks)
-        {
-            HealthySnacksModel healthySnacksModel = new HealthySnacksModel();
-            healthySnacksModel.Id = healthySnacks.Id;
-            healthySnacksModel.Name = healthySnacks.Name;
-            healthySnacksModel.Description = healthySnacks.Description;
-            healthySnacksModel.Price = healthySnacks.Price;
-            healthySnacksModel.ImageUrl = healthySnacks.ImageUrl;
-            healthySnacksModel.CategoryId = healthySnacks.CategoryId;
-
-            return healthySnacksModel;
-
-        }
-
-        public HealthySnacks MapToHealthySnacks(HealthySnacksModel healthySnacksModel)
-        {
-            HealthySnacks healthySnacks = new HealthySnacks();
-            healthySnacks.Id = healthySnacksModel.Id;
-            healthySnacks.Name = healthySnacksModel.Name;
-            healthySnacks.Description = healthySnacksModel.Description;
-            healthySnacks.Price = healthySnacksModel.Price;
-            healthySnacks.ImageUrl = healthySnacksModel.ImageUrl;
-            healthySnacks.CategoryId = healthySnacksModel.CategoryId;
-
-          
         }
     }
 }
--- HealthySnacks/Mappers/Mapper.cs
+++ HealthySnacks/Mappers/Mapper.cs
@@ -1,32 +1,40 @@
-﻿namespace HealthySnacks.Controllers
+﻿using HealthySnacks.DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using HealthySnacks.Models;
+using HealthySnacks.DataAccess.Entities;
+
+namespace HealthySnacks.Controllers
+
 {
     public class Mapper
     {
-        public HealthySnacksModel healthySnacksModel(HealthySnacks healthySnacks)
-        {
-            HealthySnacksModel healthySnacksModel = new HealthySnacksModel();
-            healthySnacksModel.Id = healthySnacks.Id;
-            healthySnacksModel.Name = healthySnacks.Name;
-            healthySnacksModel.Description = healthySnacks.Description;
-            healthySnacksModel.Price = healthySnacks.Price;
-            healthySnacksModel.ImageUrl = healthySnacks.ImageUrl;
-            healthySnacksModel.CategoryId = healthySnacks.CategoryId;
+    //    public HealthySnacksModel healthySnacksModel(HealthySnacksController healthySnacks)
+    //{
+    //    HealthySnacksModel healthySnacksModel = new HealthySnacksModel();
+    //    healthySnacksModel.Id = healthySnacks.Id;
+    //    healthySnacksModel.Name = healthySnacks.Name;
+    //    healthySnacksModel.Description = healthySnacks.Description;
+    //    healthySnacksModel.Price = healthySnacks.Price;
+    //    healthySnacksModel.ImageUrl = healthySnacks.ImageUrl;
+    //    healthySnacksModel.CategoryId = healthySnacks.CategoryId;
 
-            return healthySnacksModel;
+    //    return healthySnacksModel;
 
-        }
-
-        public HealthySnacks MapToHealthySnacks(HealthySnacksModel healthySnacksModel)
-        {
-            HealthySnacks healthySnacks = new HealthySnacks();
-            healthySnacks.Id = healthySnacksModel.Id;
-            healthySnacks.Name = healthySnacksModel.Name;
-            healthySnacks.Description = healthySnacksModel.Description;
-            healthySnacks.Price = healthySnacksModel.Price;
-            healthySnacks.ImageUrl = healthySnacksModel.ImageUrl;
-            healthySnacks.CategoryId = healthySnacksModel.CategoryId;
-
-
-        }
     }
+
+    //public HealthySnacks MapToHealthySnacks(HealthySnacksModel healthySnacksModel)
+    //{
+    //    healthySnacksModel healthySnacks = new HealthySnacks
+    //    {
+    //        Id = healthySnacksModel.Id,
+    //        Name = healthySnacksModel.Name,
+    //        Description = healthySnacksModel.Description,
+    //        Price = healthySnacksModel.Price,
+    //        ImageUrl = healthySnacksModel.ImageUrl,
+    //        CategoryId = healthySnacksModel.CategoryId
+    //    };
+
+
+    //}
+//}
 }
